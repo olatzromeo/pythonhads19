19f5c9d2ad2b2dc60570c6cdee13617b9ee24396
egibar <aeguibar006@ikasle.ehu.eus>
2017-05-15 07:50:42 +0200

    Insertar pregunta funcionando

--- Clases/Preguntas.py
+++ Clases/Preguntas.py
@@ -11,7 +11,7 @@ class Pregunta(ndb.Model):
     id_pregunta = ndb.StringProperty()
 
     # El tema de la pregunta
-    tema = ndb.IntegerProperty()
+    tema = ndb.StringProperty()
 
     # Enunciado de la pregunta
     enunciado = ndb.StringProperty()
@@ -23,10 +23,10 @@ class Pregunta(ndb.Model):
     respuesta = ndb.StringProperty()
 
     # Opcion 2 de la pregunta
-    respuesta2 = ndb.BlobKeyProperty()
+    respuesta2 = ndb.StringProperty()
 
     # Opcion 3 de la pregunta
-    respuesta3 = ndb.IntegerProperty()
+    respuesta3 = ndb.StringProperty()
 
     
 
@@ -37,7 +37,6 @@ class Pregunta(ndb.Model):
         Funcion para obtener directamente el id del objeto
     """
     def get_id(self):
-
         return self.key.id()
 
     """
@@ -46,3 +45,38 @@ class Pregunta(ndb.Model):
     def insertar_pregunta(self, id_autor, tema, enunciado,respuesta,respuesta2,respuesta3,respuestacorrecta):
         p = Pregunta(id_usuario=id_autor,id_pregunta=self.get_id(),tema=tema,enunciado=enunciado,solucion=respuestacorrecta,respuesta=respuesta,respuesta2=respuesta2,respuesta3=respuesta3)
         p.put()
+
+    def add_pregunta(self, pregunta):
+
+        m = MiPregunta()
+        m.id_pregunta=pregunta
+        #m.id_usuario=self.get_id()
+        m.put()
+
+    def del_pregunta(self, pregunta):
+        m = MiPregunta.query(MiPregunta.id_usuario == self.get_id()).filter(MiPregunta.id_pregunta == pregunta).fetch()[0]
+        m.delete()
+
+    def pregunta_guardada(self,pregunta):
+        m = Mipregunta.query(Mipregunta.id_usuario == self.get_id()).filter(Mipregunta.id_pregunta == pregunta).count()
+        print("Numero de pregunta" + str(m))
+        if m > 0:
+            return True
+        else:
+            return False
+
+    def get_preguntas(self):
+        preguntas=[]
+        mias = MiPregunta.query(MiPregunta.id_usuario==self.get_id())
+        for r in mias:
+            preguntas.append(pregunta.get_by_id(int(r.id_pregunta)))
+        return preguntas
+
+
+class MiPregunta(ndb.Model):
+
+    id_pregunta = ndb.GenericProperty()
+    id_usuario = ndb.GenericProperty()
+
+    def delete(self):
+        self.key.delete()
--- pregunta.py
+++ pregunta.py
@@ -1,28 +1,32 @@
 import webapp2
 import os
 import jinja2
-
-from Clases.Preguntas import Pregunta
+import re
+import cgi
 from BaseHandler import BaseHandler
+import uuid
+import hashlib
+from Clases.Preguntas import Pregunta
 from Clases.Usuarios import Usuario
 from google.appengine.ext import ndb
 
-
 template_dir = os.path.join(os.path.dirname(__file__), 'Paginas')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-							   autoescape = True)
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
 
 
 def render_str(template, **params):
-	t = jinja_env.get_template(template)
-	return t.render(params)
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
 
 class Handler(BaseHandler):
-	def render(self, template, **kw):
-		self.response.out.write(render_str(template, **kw))
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
 
-	def write(self, *a, **kw):
-		self.response.out.write(*a, **kw)
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
 
 '''class MainHandler(Handler):
 	def get(self):
@@ -30,52 +34,55 @@ class Handler(BaseHandler):
 		rol = self.session.get('rol')'''
 
 
-def escape_html(s):
-	return cgi.escape(s, quote=True)
-
-
 class InsertarPreguntas(Handler):
 
-	 def write_form(self, tema="", enunciado="", opcion1="", opcion2="", opcion3="", respcorrecta=""):
-		self.response.write(render_str("insertarpreguntas.html", rol='Usuario', login='si') % {"tema":tema,"enunciado":enunciado,"opcion1":opcion1,"opcion2":opcion2,"opcion3":opcion3,"respcorrecta":respcorrecta})
-   
-	def get(self):
-		self.write_form()
-   
+	def get(self, tema="", enunciado="", opcion1="", opcion2="", opcion3="", respcorrecta=""):
+		self.response.write(
+			render_str("insertarpreguntas.html", rol='Usuario', login='si') % {"tema": tema, "enunciado": enunciado,"opcion1": opcion1, "opcion2": opcion2,"opcion3": opcion3,"respcorrecta": respcorrecta})
+
 
 	def post(self):
-			tema = self.request.get('tema')
-			enunciado = self.request.get('enunciado')
-			opcion1 = self.request.get('opcion1')
-			opcion2 = self.request.get('opcion2')
-			opcion3 = self.request.get('opcion3')
-			respcorrecta= self.request.get('respcorrecta')
 
-			sani_tema = escape_html(tema)
-			sani_enunciado = escape_html(enunciado)
-			sani_opc1 = escape_html(opcion1)
-			sani_opc2 = escape_html(opcion2)
-			sani_opc3 = escape_html(opcion3)
-			sani_respcorrecta = escape_html(respcorrecta)
-			
-			usuario = self.session.get('username')
-			u = Usuario.query(Usuario.nick == usuario).fetch()[0]
-		   
-			if tema != "" and enunciado != "" and opcion1!="" and opcion2!=""and opcion3!="" and respcorrecta!="":
-				p=Pregunta()
-				p.id_pregunta=p.get_id()
-				pregunta=Pregunta.query(Pregunta.id_pregunta==p.id_pregunta).count()
-				if pregunta==0:
-				   p.tema=tema
-				   p.enunciado=enunciado
-				   p.solucion=respcorrecta
-				   p.respuesta=opcion1
-				   p.respuesta2=opcion2
-				   p.respuesta3=opcion3
-				   p.addpregunta(p)
-				   self.render("errores.html", rol='Usuario', login='si', message='Pregunta creada correctamente',)
-			else:
-				self.render("errores.html", rol='Usuario', login='si', message='Todos los campos deben de ser rellenados para insertar una nueva pregunta',)
+		def escape_html(s):
+			return cgi.escape(s, quote=True)
+
+		tema = self.request.get('tema')
+		enunciado = self.request.get('enunciado')
+		opcion1 = self.request.get('opcion1')
+		opcion2 = self.request.get('opcion2')
+		opcion3 = self.request.get('opcion3')
+		respcorrecta = self.request.get('respcorrecta')
+		sani_tema = escape_html(tema)
+		sani_enunciado = escape_html(enunciado)
+		sani_opc1 = escape_html(opcion1)
+		sani_opc2 = escape_html(opcion2)
+		sani_opc3 = escape_html(opcion3)
+		sani_respcorrecta = escape_html(respcorrecta)
+
+		usuario = self.session.get('username')
+		u = Usuario.query(Usuario.nick == usuario).fetch()[0]
+
+		if tema != "" and enunciado != "" and opcion1 != "" and opcion2 != "" and opcion3 != "" and respcorrecta != "":
+			p = Pregunta()
+			'''p.id_pregunta = p.get_id()
+			pregunta = Pregunta.query(Pregunta.id_pregunta == p.id_pregunta).count()
+			if  pregunta == 0:'''
+			p.tema = tema
+
+			p.id_pregunta = self.request.get('id')
+			p.id_usuario = u.get_id()
+			p.enunciado = enunciado
+			p.solucion = respcorrecta
+			p.respuesta = opcion1
+			p.respuesta2 = opcion2
+			p.respuesta3 = opcion3
+			p.put()
+			#p.add_pregunta(p)
+			self.render("errores.html", rol='Usuario', login='si', message='Pregunta creada correctamente', )
+
+		else:
+			self.render("errores.html", rol='Usuario', login='si',
+            	message='Todos los campos deben de ser rellenados para insertar una nueva pregunta', )
 
 '''class VisualizarPreguntas(Handler):
 	def get(self):
@@ -89,26 +96,26 @@ class InsertarPreguntas(Handler):
 		preguntas = Preguntas.query().fetch()
 		self.render("visualizarpreguntas.html", rol=rol, login=login, preguntas=preguntas)'''
 
-
+'''
 class RegisterHandler(Handler):
-	def get(self):
-		rol = self.session.get('rol')
-		if not rol:
-			rol = "Anonimo"
-		if rol == "Anonimo":
-			self.write("No tienes permiso")
-		else:
-			self.render_upload("insertarpreguntas.html", rol=rol, login='si')
-
+    def get(self):
+        rol = self.session.get('rol')
+        if not rol:
+            rol = "Anonimo"
+        if rol == "Anonimo":
+            self.write("No tienes permiso")
+        else:
+            self.render_upload("insertarpreguntas.html", rol=rol, login='si')
+'''
 
 config = {}
 config['webapp2_extras.sessions'] = {
-	'secret_key': 'my-super-secret-key',
+    'secret_key': 'my-super-secret-key',
 }
 
 app = webapp2.WSGIApplication([
-	('/pregunta/insertarpreguntas', InsertarPreguntas),
-	#('/pregunta/crear', RegisterHandler),
-	('/iniciosesion', InicioSesion),
-	('/cerrarsesion', CerrarSesion)
-],config=config, debug=True)
+    ('/pregunta/insertarpreguntas', InsertarPreguntas),
+    # ('/pregunta/crear', RegisterHandler),
+    #('/iniciosesion', InicioSesion),
+    #('/cerrarsesion', CerrarSesion)
+], config=config, debug=True)
